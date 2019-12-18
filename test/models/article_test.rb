5d86f83c2def24e8cf1d5c705039f0e0a8c2dc60
Kazuki Horie <kazukihorie.jp@gmail.com>
2019-12-18 18:52:50 +0900

    Add Article Model

---
+++ test/models/article_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class ArticleTest < ActiveSupport::TestCase
+  test "should not save article without title" do
+    article = Article.new(
+      title: 'test'
+    )
+  
+    assert_not article.save
+  end
+
+  test "should not save article without description" do
+    article = Article.new(
+      description: 'testtesttesttesttest'
+    )
+
+    assert_not article.save
+  end
+end
