c7b30188bfdebe1f06d378b7312020728d3578c2
Kazuki Horie <kazukihorie.jp@gmail.com>
2019-12-19 12:28:24 +0900

    Modify config CORS

--- config/application.rb
+++ config/application.rb
@@ -36,8 +36,11 @@ module RailsBlogApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
+        origins 'http://localhost:9004', 'https://editor.swagger.io/'
+
+        resource '*',
+          :headers => :any,
+          :methods => [:get, :post, :put, :delete, :options]
       end
     end
   end
